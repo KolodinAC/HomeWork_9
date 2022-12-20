0178e9809267adc6cbf7d6711d578cff1daa122b
Anton Kolodin <anton.kolodin2012@yandex.ru>
2022-12-20 12:51:21 +0300

    task 64 done

--- Task064/Program.cs
+++ Task064/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+using static Common.Helper;
+
+Console.WriteLine();
+int userNumber = UserInput("Введите желаемое число: ");
+Console.WriteLine();
+Console.WriteLine($"Список натуральных чисел от {userNumber} до 1 : ");
+Console.WriteLine();
+Console.WriteLine(StringRecursion(number: userNumber));
+
+
+
+string StringRecursion(int number)
+{
+    if (number == 1) return number.ToString();
+
+    return number + " " + StringRecursion(number - 1);
+}
